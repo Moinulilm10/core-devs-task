657aec0b1e7226fee7052ea504dd7116729b339c
Moinulilm10 <moinulilm10@gmail.com>
2023-05-03 22:38:18 +0600

    cleaning in hitToast file

--- src/helpers/hitToast.js
+++ src/helpers/hitToast.js
@@ -1,19 +1,4 @@
 import { toast } from 'react-toastify';
-// const hitToast = (variant, message) => {
-//     // if (!['success', 'error'].includes(variant)) throw Error();
-//     if (!['success', 'error'].includes(variant)) throw new Error(`Invalid variant value "${variant}"`);
-//     toast[`${variant}`](message, {
-//         position: "top-right",
-//         autoClose: 3000,
-//         hideProgressBar: false,
-//         closeOnClick: true,
-//         pauseOnHover: true,
-//         draggable: true,
-//         progress: undefined,
-//     });
-// }
-// export default hitToast;
-
 const hitToast = (variant, message) => {
   if (!['success', 'error'].includes(variant)) {
     console.warn(`Invalid variant value "${variant}", defaulting to "error".`);
